23b312be0c4c0c7c46ef93c45bbc25f4c93c14c4
veroo <verooedward@gmail.com>
2024-12-14 03:18:12 +0200

    reorderpoint done.

--- src/main/java/com/supermacro/InventoryEmp.java
+++ src/main/java/com/supermacro/InventoryEmp.java
@@ -21,13 +21,25 @@ public class InventoryEmp extends Employee{
     public void deleteProduct(Product p){
             inventory.remove(p);
     }
-    public void updateProduct(Product p){
-        for(int i=0;i< inventory.size();i++){
-            if(inventory.get(i).getProductId()==p.getProductId())
-                inventory.set(i,p);
-        }
 
-    }
+//    public void updateProduct(int choice){
+//        for(int i=0; i<inventory.size();i++){
+//            if(inventory.get(i).getProductId()==p.getProductId())
+//
+//        }
+//        switch (choice){
+//            case 1:
+//
+//                        inventory.set(i,p).setPrice();
+//
+//        }
+
+//        for(int i=0;i< inventory.size();i++){
+//            if(inventory.get(i).getProductId()==p.getProductId())
+//                inventory.set(i,p);
+
+
+
     public Product searchProduct(String name){
         for(int i=0; i< inventory.size(); i++) {
             if(name== inventory.get(i).getName())
@@ -38,9 +50,17 @@ public class InventoryEmp extends Employee{
 public ArrayList<Product> listProduct(){
         return inventory;
 }
-public ArrayList<Product> productProblems(){
-        ArrayList<Products>
+
+public ArrayList<Product> reorderPoint(int reorderQuantity){
+        ArrayList<Product> shortageProducts= new ArrayList<>();
+        for(int i=0; i<inventory.size(); i++){
+            if(inventory.get(i).getQuantity()<= reorderQuantity)
+                shortageProducts.add(inventory.get(i));
+        }
+        return shortageProducts;
+
 }
+//public void updateProduct(int choice,)
 
 
 
